dac482e7adb0c38af8a1f1ee2868c037223fc4a3
Safiyat Reza <reza.safiyat@gmail.com>
2014-09-25 17:48:22 +0530

    Update minor.py

--- minor.py
+++ minor.py
@@ -1,11 +1,24 @@
 from functions import *
 import numpy
+import os
 from scikits.audiolab import Format, Sndfile
-#1
-data1, S1, T1 = wavopen("sounds/02aashiqui150-less.wav")		#Cover audio file
-data2, S2, T2 = wavopen("sounds/02flute04-less.wav")		#Target audio file
 
-theta = 0.08
+file1 = raw_input("Enter the cover audio file name: ")
+file2 = raw_input("Enter the target audio file name: ")
+
+fs = int(raw_input("Enter the desired sampling rate: "))
+
+temp1 = 'ffmpeg -v quiet -i ' + file1 + ' -ac 1 -ar ' + str(fs) + ' cover.wav'
+temp2 = 'ffmpeg -v quiet -i ' + file2 + ' -ac 1 -ar ' + str(fs) + ' target.wav'
+os.system(temp1)
+os.system(temp2)
+
+#1
+data1, S1, T1 = wavopen('cover.wav')		#Cover audio file
+data2, S2, T2 = wavopen('target.wav')		#Target audio file
+
+
+theta = float(raw_input("Enter the value of theta: "))
 if T1 < 3 * T2 / theta:
 	print "Cover audio file length is insufficient!!! Please use an audio file atleast ", 3*T2/theta, " seconds in length."
 	quit()
@@ -13,24 +26,18 @@ if T1 < 3 * T2 / theta:
 #2
 target_split = splitArray(data2, S2, theta)
 
-#3
+#3 #5
 gap_in_s = int(numpy.floor(T1*theta/T2))
 gap = gap_in_s * S1
-#phase_shift_in_s = int(numpy.floor(theta*S2/S1))
-#phase_shift = phase_shift_in_s * S1;
-phase_shift = int(numpy.floor(theta*S2));
+
 i=0
 j=gap
-vacant=[-1]*phase_shift			#fill it up with -1
-print "Length of data1 before phaseshift: ", len(data1)
-while i < len(target_split):
-	for a in vacant:
-		data1.insert(j, a)
-	print i, ". New sample sequence: data1[", j-2, ":", j+phase_shift+2, "] : ", data1[j-2:j+phase_shift+2]
-	j+=gap+phase_shift
-	i+=1
 
-print "Length of data1 after phaseshift: ", len(data1)
+
+while len(target_split):
+	data1[j:0] = target_split[0]
+	j+=gap+len(target_split[0])
+	target_split.pop(0)
 
 #4
 
@@ -39,19 +46,9 @@ data1[1]=S2^len(data1)
 data1[2]=T2^len(data1)
 data1[3]=theta
 
-#5
-while len(target_split):
-	i = data1.index(-1)
-	j = 0
-	while j < len(target_split[0]):
-		data1[i] = target_split[0][j]
-		i+=1
-		j+=1
-	target_split.pop(0)
-
 #6
 op=numpy.asarray(data1)
 format = Format('wav')
-f = Sndfile("sounds/stego_file.wav", 'w', format, 1, 11025)
+f = Sndfile("stego_file.wav", 'w', format, 1, fs)
 f.write_frames(op)
 f.close()
